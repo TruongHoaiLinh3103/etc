88fe9bad4b19e19647b6f114236b5e4096e8f28c
Anikey <eagleremlinh@gmail.com>
2023-12-09 12:45:46 +0700

    C

--- src/Views/App/App.js
+++ src/Views/App/App.js
@@ -22,13 +22,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCXH20KzejwfT3yZwVMf8pN2ivxh2OBSNk",
-  authDomain: "etc-2023-cb332.firebaseapp.com",
-  projectId: "etc-2023-cb332",
-  storageBucket: "etc-2023-cb332.appspot.com",
-  messagingSenderId: "983189452128",
-  appId: "1:983189452128:web:ed925144084877caf95a05",
-  measurementId: "G-41V400XBSP"
+  apiKey: "AIzaSyDq_SAqXN6PnPp53dcrTKcuXQbEFtTi8hs",
+  authDomain: "etc-31032003.firebaseapp.com",
+  projectId: "etc-31032003",
+  storageBucket: "etc-31032003.appspot.com",
+  messagingSenderId: "751434193660",
+  appId: "1:751434193660:web:9d0ca8a15f0f9bd5b1d579",
+  measurementId: "G-BYSCV7BD9Z"
 };
 
 // Initialize Firebase
