b889dda1f62981428fdd2ac7cfd16f04a7ad350b
Anikey <eagleremlinh@gmail.com>
2023-12-09 12:29:55 +0700

    B

--- src/Views/App/App.js
+++ src/Views/App/App.js
@@ -13,6 +13,28 @@ import "../../Style/Basic.css";
 import '../../Style/grid.css';
 import ServicePage from "../../Components/ServicePage/ServicePage";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCXH20KzejwfT3yZwVMf8pN2ivxh2OBSNk",
+  authDomain: "etc-2023-cb332.firebaseapp.com",
+  projectId: "etc-2023-cb332",
+  storageBucket: "etc-2023-cb332.appspot.com",
+  messagingSenderId: "983189452128",
+  appId: "1:983189452128:web:ed925144084877caf95a05",
+  measurementId: "G-41V400XBSP"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 function App() {
   return (
     <Router>
